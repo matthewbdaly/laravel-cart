99162c3ea04dbb4911aa6e870ec9017c8e9df190
Matthew Daly <matthewbdaly@gmail.com>
2017-11-15 11:52:29 +0000

    Added first pass at cart test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,11 +3,18 @@
 namespace Tests;
 
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use Mockery as m;
 
 class TestCase extends BaseTestCase
 {
 	protected function getPackageProviders($app)
 	{
 		return ['Matthewbdaly\LaravelCart\Providers\CartServiceProvider'];
+    }
+
+    public function tearDown()
+    {
+        m::close();
+        parent::tearDown();
     }
 }
--- tests/Unit/Services/CartTest.php
+++ tests/Unit/Services/CartTest.php
@@ -3,7 +3,20 @@
 namespace Tests\Unit\Services;
 
 use Tests\TestCase;
+use Matthewbdaly\LaravelCart\Services\Cart;
+use Mockery as m;
 
 class CartTest extends TestCase
 {
+    public function testCanAddItemToCart()
+    {
+        $session = m::mock('Illuminate\Contracts\Session\Session');
+        $cart = new Cart($session);
+        $cart->insert([
+            'qty'     => 1,
+            'price'   => 39.95,
+            'name'    => 'T-Shirt',
+            'options' => array('Size' => 'L', 'Color' => 'Red')
+        ]);
+    }
 }
