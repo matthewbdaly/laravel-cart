3e24c1b3a873d9ae19c9ecf62fb76866e144fc16
Matthew Daly <matthewbdaly@gmail.com>
2017-11-15 16:30:51 +0000

    Commented unique ID class

--- src/Contracts/Services/UniqueId.php
+++ src/Contracts/Services/UniqueId.php
@@ -4,5 +4,10 @@ namespace Matthewbdaly\LaravelCart\Contracts\Services;
 
 interface UniqueId
 {
+    /**
+     * Generate unique ID
+     *
+     * @return string
+     */
     public function get();
 }
--- src/Services/UniqueId.php
+++ src/Services/UniqueId.php
@@ -4,8 +4,16 @@ namespace Matthewbdaly\LaravelCart\Services;
 
 use Matthewbdaly\LaravelCart\Contracts\Services\UniqueId as UniqueIdContract;
 
+/**
+ * Generates unique ID's
+ */
 class UniqueId implements UniqueIdContract
 {
+    /**
+     * Generate unique ID
+     *
+     * @return string
+     */
     public function get()
     {
         return uniqid();
