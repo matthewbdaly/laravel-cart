6fb8b2b0578f05805f7be991bc0c041aa24dc317
Matthew Daly <matthewbdaly@gmail.com>
2017-11-15 11:40:57 +0000

    Added service provider

---
+++ src/Providers/CartServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Matthewbdaly\LaravelCart\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Service provider for flat pages
+ */
+class FlatpageServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
